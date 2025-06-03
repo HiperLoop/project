027578d3d2c0c17b4ee6e6d210f8bb4b330391e9
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-06-03 13:41:20 +0200

    degfine class Body

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+class Body:
+    """Class representing a celestial body with mass, radius, position, and velocity."""
+
+    dimension = 2  # Number of dimensions (3D space)
+
+    def __init__(self, name, mass, radius, position, velocity):
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        self.position = np.array(position)
+        self.velocity = np.array(velocity)
+
+    def __repr__(self):
+        return f"Body(name={self.name}, mass={self.mass}, radius={self.radius}, position={self.position}, velocity={self.velocity})"
