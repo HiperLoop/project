f7b9d5e7df5a29a4fe09010c444872fefe30da11
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-06-05 16:11:38 +0200

    added animate from data functionality to Animation class

--- src/animation.py
+++ src/animation.py
@@ -1,38 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from loaders import load_simulation_from_file
+from body import Body
 
 class Animation:
     """Class for animating the simulation of n bodies."""
 
-    def __init__(self, simulation, plot_size=6, plot_dimensions=1, frame_rate=100, sim_duration=1000):
+    def __init__(self, plot_size=6, plot_dimensions=1, frame_rate=100, data_from_file=False, **kwargs):
         """Initialize the animation with the bodies and simulation duration."""
-        self.simulation = simulation
+        self.data_from_file = data_from_file
+        if data_from_file:
+            self.dimension = kwargs.get('dimension', 2)
+            Body.dimension = self.dimension
+            self.bodies, self.data = load_simulation_from_file(kwargs.get('file_name', None), self.dimension)
+        else:
+            self.simulation = kwargs.get('simulation', None)
+            self.bodies = self.simulation.bodies
+            self.dimension = self.simulation.dimension
+
+        self.step = 0
         self.plot_size = plot_size
         self.plot_dimensions = plot_dimensions
         self.frame_rate = frame_rate
-        self.sim_duration = sim_duration
         self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(projection='3d' if simulation.dimension == 3 else None)
+        self.ax = self.fig.add_subplot(projection='3d' if self.dimension == 3 else None)
         self.fig.set_figheight(plot_size)
         self.fig.set_figwidth(plot_size)
-        if simulation.dimension == 3:
+        if self.dimension == 3:
             self.ax.set(xlim=[-plot_dimensions, plot_dimensions], ylim=[-plot_dimensions, plot_dimensions], zlim=[-plot_dimensions, plot_dimensions], xlabel='X', ylabel='Y', zlabel='Z')
         else:
             self.ax.set(xlim=[-plot_dimensions, plot_dimensions], ylim=[-plot_dimensions, plot_dimensions], xlabel='X', ylabel='Y')
         self.global_scats, self.global_lines = self.plot_init()
+
         self.animate()
 
     def plot_init(self):
         """Initializes the plot with scatter points and lines for each body."""
         scats = []
         lines = []
-        for body in self.simulation.bodies:
-            if self.simulation.dimension == 3:
+        for body in self.bodies:
+            if self.dimension == 3:
                 scats.append(self.ax.scatter(body.position[0], body.position[1], body.position[2], c=body.display_colour, s=5, label=body.name))
                 lines.append(self.ax.plot(body.position[0], body.position[1], body.position[2], zdir='z', c=body.display_colour, alpha=0.2, label=f'{body.name} orbit')[0])
             else:
-                scats.append(self.ax.scatter(0, 0, c=body.display_colour, s=5, label=body.name))
+                scats.append(self.ax.scatter(body.position[0], body.position[1], c=body.display_colour, s=5, label=body.name))
                 lines.append(self.ax.plot(body.position[0], body.position[1], c=body.display_colour, alpha=0.2, label=f'{body.name} orbit')[0])
         self.ax.legend()
         return scats, lines
@@ -55,16 +67,27 @@ class Animation:
 
     def draw_bodies(self, scats, lines):
         """Updates the scatter and line plots for all bodies."""
-        for i, body in enumerate(self.simulation.bodies):
+        for i, body in enumerate(self.bodies):
             scats[i], lines[i] = self.update_body_plot(body, scats[i], lines[i])
         return scats, lines
 
+    def update_bodies(self):
+        if self.data_from_file:
+            self.step = min(self.step, len(self.data) - 1)
+            row = self.data[self.step]
+            for i, body in enumerate(self.bodies):
+                body.position = [float(row[(i*2) * 3 + j]) for j in range(self.dimension)]
+                body.velocity = [float(row[((i*2)+1) * 3 + j]) for j in range(self.dimension)]
+        else:
+            continuous_evolve = self.simulation.solve_velocities(0).y
+
     def update(self, frame):
         """Update function for the animation."""
-        continuous_evolve = self.simulation.solve_velocities(0, 1000).y
+        self.update_bodies()
         graphs = [self.draw_bodies(self.global_scats, self.global_lines)]
+        self.step += 1
         return graphs
 
     def animate(self):
-        ani = animation.FuncAnimation(fig=self.fig, func=self.update, frames=100000, interval=1000//self.frame_rate, repeat=False)
+        ani = animation.FuncAnimation(fig=self.fig, func=self.update, frames=100000, interval=1000//self.frame_rate, repeat=True)
         plt.show()
