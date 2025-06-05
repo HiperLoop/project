b47e6eb5c2bdc94eadbace8dcc7b1fe3d5af0513
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-06-05 16:12:18 +0200

    testing of animation loading

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ def main():
     #figure_eight_configureation_3D()
     #solar_system()
     #solar_system_3D()
-    comet_solar_system()
-
+    #comet_solar_system()
+    system_from_file("2025-06-05_16-01-10.csv", dimension=2) # 2D load test
+    #system_from_file("2025-06-05_16-01-10.csv", dimension=3) # 3D load test
+    
 main()
