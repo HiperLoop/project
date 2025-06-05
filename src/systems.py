4d033e88e1e2659b749d5278c07050a927fb4844
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-06-05 13:27:20 +0200

    testing

--- src/systems.py
+++ src/systems.py
@@ -1,6 +1,7 @@
 from loaders import *
 from simulation import Simulation
 from animation import Animation
+from measures import *
 
 def figure_eight_configureation():
     """Configure the simulation for the figure-eight configuration."""
@@ -26,7 +27,7 @@ def solar_system():
     sun = load_body_from_custom_csv('./objects/custom_objects.csv')[0]
     bodies = [sun] + planets
     # Initialize the simulation
-    sim = Simulation(bodies, dimension=2, G=1, norming_distance=149, norming_velocity=29.8, norm=True, reverse=False, time_step=0.1)
+    sim = Simulation(bodies, dimension=2, G=1, norming_distance=149.6, norming_velocity=29.8, norm=True, reverse=False, time_step=0.1)
     # Initialize the animation
     anim = Animation(sim, plot_size=6, plot_dimensions=40, sim_duration=1000, frame_rate=100)
 
@@ -38,11 +39,13 @@ def comet_solar_system():
     sun = load_body_from_custom_csv('./objects/custom_objects.csv')[0]
     # Load comet from the custom CSV file
     comet = load_body_from_custom_csv('./objects/custom_objects.csv')[-1]
-    bodies = [sun] + [comet]# + planets
+    earth = load_body_from_csv('./objects/planets.csv', dimension=2)[3]  # Load Earth for reference
+    bodies = [sun] + [earth]# + planets
+
     # Initialize the simulation
-    sim = Simulation(bodies, dimension=2, G=1, norming_distance=149, norming_velocity=29.8, norm=True, reverse=False, time_step=0.1, save_to_file=True)
+    sim = Simulation(bodies, dimension=2, G=1, norming_distance=149.6, norming_velocity=29.8, norm=True, reverse=False, time_step=0.01, save_to_file=False)
     # Initialize the animation
-    anim = Animation(sim, plot_size=6, plot_dimensions=15, sim_duration=1000, frame_rate=200)
+    anim = Animation(sim, plot_size=6, plot_dimensions=15, sim_duration=1000, frame_rate=2000)
 
 def solar_system_3D():
     """Configure the simulation for the solar system."""
