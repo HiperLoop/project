0021e6ecdd836f0043a29a8f38aacb128f442673
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-06-05 16:11:18 +0200

    fixed simulation writer and added simulation loader

--- src/loaders.py
+++ src/loaders.py
@@ -40,10 +40,17 @@ def write_simulation_to_file_step(file, bodies):
     """Write the current state of the simulation to a CSV file. DOES NOT CLOSE FILE!"""
     dimension = Body.dimension
     write_row = ""
-    for body in bodies:
+    for i, body in enumerate(bodies):
         position = body.position if dimension == 3 else np.append(body.position, 0)
         velocity = body.velocity if dimension == 3 else np.append(body.velocity, 0)
-        write_row += (",".join([str(coord) for coord in np.append(position, velocity)])) + ","
+        write_row += (",".join([str(coord) for coord in np.append(position, velocity)])) + ("," if i != len(bodies)-1 else "")
+    file.write(write_row + "\n")
+
+def write_simulation_to_file_step_y(file, y):
+    """Write the current state of the simulation to a CSV file. DOES NOT CLOSE FILE!"""
+    dimension = Body.dimension
+    number_of_bodies = y.shape[1] // (dimension*2)
+    write_row = ("\n".join([",".join([",".join([str(number) for number in np.concatenate([y[i, j*dimension:(j+1)*dimension] if dimension == 3 else np.append(y[i, j*dimension:(j+1)*dimension], 0), y[i, j*dimension+dimension:(j+1)*dimension+dimension] if dimension == 3 else np.append(y[i, j*dimension+dimension:(j+1)*dimension+dimension], 0)])]) for j in range(number_of_bodies)]) for i in range(y.shape[0])]))
     file.write(write_row + "\n")
      
 def write_simulation_to_file_init(bodies):
@@ -61,10 +68,31 @@ def write_simulation_to_file_init(bodies):
     file.write("# ================================================================================================\n")
     file.write("#\n")
     file.write("#" + "name,mass,radius,colour,_,_," * len(bodies) + "\n")
-    file.write(",".join([f"{body.name},{body.mass},{body.radius},{body.display_colour},0,0" for body in bodies]) + "\n")
+    file.write(",".join([f"{body.name},{body.mass},{body.radius},{body.display_colour[1:]},0,0" for body in bodies]) + "\n")
     file.write("#" + "x,y,z,vx,vy,vz," * len(bodies) + "\n")
     file.close()
     file = open(file_name, "a")
     write_simulation_to_file_step(file, bodies)
     file.close()
     return file_name
+
+def load_simulation_from_file(filename, dimension=2):
+    """Load simulation data from a CSV file."""
+    text_bodies = np.genfromtxt(SIMULATIONS_PATH+filename, delimiter=',', dtype=None, encoding=None)
+    #print(text_bodies.shape)
+    bodies = []
+    characteristics = text_bodies[0]
+    initial_vectors = text_bodies[1]
+    vector_data = text_bodies[2:]
+    print(vector_data[0][0])
+    vec_size = 6
+    for i in range(len(characteristics) // vec_size):
+        name = characteristics[i*vec_size]
+        mass = characteristics[i*vec_size + 1]
+        radius = float(characteristics[i*vec_size + 2])
+        colour = "#" + characteristics[i*vec_size + 3]
+        position = np.array([float(initial_vectors[i*vec_size + j]) for j in range(dimension)])
+        velocity = np.array([float(initial_vectors[i*vec_size + j + dimension]) for j in range(dimension)])
+        body = Body(name, colour, float(mass), float(radius), position, velocity)
+        bodies.append(body)
+    return bodies, vector_data
