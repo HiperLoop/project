7889145f89358282a2a7f2b3118cc1788f334124
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-06-06 13:42:55 +0200

    further system class testing

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ def main():
     #system_from_file("2025-06-05_16-28-24.csv", dimension=2)
     #system_from_file("2025-06-05_16-01-10.csv", dimension=3) # 3D load test
     #figure_eight_configureation(False)
-    test_config.run()
+    #test_config.run()
+    centauri_system.run()
 
 #write_body_to_file()
 
--- src/systems.py
+++ src/systems.py
@@ -26,7 +26,10 @@ class System:
         self.sim.reverse_velocities()
         self.run()
 
-test_config = System(animate=True, from_file=False, bodies=load_body_from_custom_csv('custom_objects.csv', dimension=2)[-4:-1], simulation_parameters=Simualtion_parameters(dimension=2, gravitational_constant=3, step_time=0.1), animation_parameters=Animation_parameters(plot_axis_limits=3, plot_dimension=2))
+#test_config = System(animate=True, from_file=False, bodies=load_body_from_custom_csv('custom_objects.csv', dimension=2)[-4:-1], simulation_parameters=Simualtion_parameters(dimension=2, gravitational_constant=3, step_time=0.1), animation_parameters=Animation_parameters(plot_axis_limits=3, plot_dimension=2))
+centauri_bodies=load_body_from_custom_csv('centauri.csv', dimension=3)
+centauri_system = System(animate=True, from_file=False, bodies=centauri_bodies, simulation_parameters=Simualtion_parameters(dimension=3, do_norming=True, distance_norm=1, step_time=0.1), animation_parameters=Animation_parameters(plot_axis_limits=15, plot_dimension=3, frame_rate=200))
+
 
 def figure_eight_configureation(animate=True):
     """Configure the simulation for the figure-eight configuration."""
