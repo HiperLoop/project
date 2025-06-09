6b2e6cb3cef69e224468be89334b23daafaefb9b
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-06-09 11:27:49 +0200

    testing

--- src/systems.py
+++ src/systems.py
@@ -14,7 +14,7 @@ class System:
             if animate:
                 self.animation = Animation(animation_params, data_from_file=True, file_name=kwargs.get('file_name', None))
         else:
-            self.sim = Simulation(kwargs.get('bodies', None), simulation_params, reverse=False, save_to_file=False, auto_run=not animate)
+            self.sim = Simulation(kwargs.get('bodies', None), simulation_params, reverse=False, save_to_file=True, auto_run=not animate)
             if animate:
                 self.animation = Animation(animation_params, simulation=self.sim)
     
@@ -27,9 +27,12 @@ class System:
         self.run()
 
 #test_config = System(animate=True, from_file=False, bodies=load_body_from_custom_csv('custom_objects.csv', dimension=2)[-4:-1], simulation_parameters=Simualtion_parameters(dimension=2, gravitational_constant=3, step_time=0.1), animation_parameters=Animation_parameters(plot_axis_limits=3, plot_dimension=2))
-centauri_bodies=load_body_from_custom_csv('centauri.csv', dimension=3)
-centauri_system = System(animate=True, from_file=False, bodies=centauri_bodies, simulation_parameters=Simualtion_parameters(dimension=3, do_norming=True, distance_norm=1, step_time=0.1), animation_parameters=Animation_parameters(plot_axis_limits=15, plot_dimension=3, frame_rate=200))
+#centauri_bodies=load_body_from_custom_csv('centauri.csv', dimension=3)
+#centauri_system = System(animate=True, from_file=False, bodies=centauri_bodies, simulation_parameters=Simualtion_parameters(dimension=3, do_norming=True, distance_norm=1, step_time=0.1), animation_parameters=Animation_parameters(plot_axis_limits=15, plot_dimension=3, frame_rate=200))
 
+def from_file_test(file_name):
+    sys = System(animate=True, from_file=True, animation_parameters=Animation_parameters(plot_axis_limits=2, plot_dimension=3, frame_rate=80), file_name=file_name)
+    sys.run()
 
 def figure_eight_configureation(animate=True):
     """Configure the simulation for the figure-eight configuration."""
@@ -66,10 +69,14 @@ def solar_system2():
     #load Sun from the custom CSV file
     sun = load_body_from_custom_csv('custom_objects.csv', dimension=3)[0]
     bodies = [sun] + planets
+    solar_system = System(animate=False, from_file=False, bodies=bodies, simulation_parameters=Simualtion_parameters(dimension=3, do_norming=True, step_time=0.1, precision=10, step_iterations=100000), animation_parameters=Animation_parameters(plot_axis_limits=40, plot_dimension=3, frame_rate=40))
+    solar_system.run()
     # Initialize the simulation
-    sim = Simulation(bodies, dimension=3, G=1, norming_distance=149.6, norming_velocity=29.8, norm=True, reverse=False, precision=1000, time_step=0.1, save_to_file=False)
+    #sim = Simulation(bodies, dimension=3, G=1, norming_distance=149.6, norming_velocity=29.8, norm=True, reverse=False, precision=1000, time_step=0.1, save_to_file=False)
     # Initialize the animation
-    anim = Animation(simulation=sim, plot_size=6, plot_dimensions=40, frame_rate=100)
+    #anim = Animation(simulation=sim, plot_size=6, plot_dimensions=40, frame_rate=100)
+
+
 
 def comet_solar_system():
     """Configure the simulation for the solar system."""
