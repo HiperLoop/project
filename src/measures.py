922f2b57cc8d1949c8e60dfdbf9e06d3d17a2ba4
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-06-11 15:02:40 +0200

    redid calculate angle and added calculate distance

--- src/measures.py
+++ src/measures.py
@@ -4,11 +4,11 @@ def relative_position(body1, body2):
         """Calculate the relative position vector from body1 to body2."""
         return body2.position - body1.position
 
-def calculate_angle(original_vector, bodies):
+def calculate_angle(body, new_magnitude):
         '''calculate the relative angle between the original body vector and their current relative position in radians'''
-        new_vector = relative_position(bodies[0], bodies[1])
-        dot = np.dot(original_vector, new_vector)
-        original_magnitude = np.linalg.norm(original_vector)
-        new_magnitude = np.linalg.norm(new_vector)
-        print((np.arccos(dot / (original_magnitude * new_magnitude))))
-        return ((np.arccos(dot / (original_magnitude * new_magnitude))))
+        dot = np.dot(body.position, body.initial_vector)
+        return ((np.arccos(dot / (body.initial_vector_magnitude * new_magnitude))))
+
+def update_limit_distances(body, current_distance):
+        if body.max_distance < current_distance: body.max_distance = current_distance
+        if body.min_distance > current_distance: body.min_distance = current_distance
